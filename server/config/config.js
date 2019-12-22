334a72736e160893e39060ad063e5f72e5c0ed33
Luis Briceño <luijo265@gmail.com>
2019-12-22 10:33:33 -0500

    Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://admin:admin@localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://luijo:ybdpYpKriNtmcpdf@cluster0-wewsz.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.urlDB = urlDB
