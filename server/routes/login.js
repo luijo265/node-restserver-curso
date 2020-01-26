e3af9de3645d997a2edf1c1937172c13e36b3338
Luis Briceño <luijo265@gmail.com>
2020-01-26 11:43:31 -0500

    google Sign-In

--- server/routes/login.js
+++ server/routes/login.js
@@ -41,31 +41,95 @@ app.post('/login', (req, res) => {
 })
 
 // Configuraciones de google
-async function verify( token ) {
+async function verify(token) {
 
     const ticket = await client.verifyIdToken({
-      idToken: token,
-      audience: process.env.CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        idToken: token,
+        audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
     });
     const payload = ticket.getPayload();
 
-    console.log(payload.name)
-    console.log(payload.email)
-    console.log(payload.picture)
+    const { name: nombre, email, picture: img } = payload
+
+    return {
+        nombre,
+        email,
+        img,
+        google: true
+    }
 
 }
 
-app.post('/google', (req, res) => {
+app.post('/google', async(req, res) => {
 
     let body = req.body
 
-    verify(body.idtoken)
-        .catch( console.log )
+    const googleUser = await verify(body.idtoken)
+        .catch(e => {
+            return resError(res, e, 403)
+        })
 
-    res.json({
-        ok:true,
-        body
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+
+        if (err) {
+            return resError(res, e, 500)
+        }
+
+        if (usuarioDB) {
+
+            if (usuarioDB.google === false) {
+                let error = {
+                    message: 'Debe usar su autenticación normal'
+                }
+                return resError(res, error, 400)
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+            }
+
+        } else {
+            // Siel usuario no existe en la base de datos
+            let usuario = new Usuario()
+
+            usuario.nombre = googleUser.nombre
+            usuario.email = googleUser.email
+            usuario.img = googleUser.img
+            usuario.google = googleUser.google
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+
+                if (err) {
+                    return resError(res, err, 500)
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, { expiresIn: process.env.CADUCIDAD_TOKEN });
+
+                res.json({
+                    ok: true,
+                    usuario: usuarioDB,
+                    token
+                })
+
+            })
+
+        }
+
     })
+
+    // res.json({
+    //     ok: true,
+    //     usuario: googleUser
+    // })
 
 });
 
