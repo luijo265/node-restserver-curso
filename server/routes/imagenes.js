4eb61069862b40173de5f0218de042651eba2373
Luis Briceño <luijo265@gmail.com>
2020-02-18 14:27:37 -0500

    nivel 162

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -1,15 +1,29 @@
 const express = require('express')
 
 const fs = require('fs')
+const path = require('path')
 
 let app = express()
 
 
-app.get('/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', (req, res) => {
 
 	let { tipo, img } = req.params
 
 	let pathImg = `./uploads/${ tipo }/${ img }`
+
+	let imagePath = path.resolve(__dirname, '../uploads/${ tipo }/${ img }')
+
+	if ( fs.existsSync( imagePath ) ) {
+
+		res.sendFile(imagePath)
+
+	}else{
+
+		res.sendFile(path.resolve(__dirname, '../assets/no-image.png'))
+
+	}
+
 
 	
 
